f770f8e68588a0308a49314e39d72abc76cf8ccf
Paul Hill <zikalify@gmail.com>
2024-07-29 22:23:51 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'my-app-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/icon-192x192.png',
-  '/icon-512x512.png'
+  './',
+  './index.html',
+  './styles.css',
+  './script.js',
+  './icon-192x192.png',
+  './icon-512x512.png'
 ];
 
 self.addEventListener('install', function(event) {
