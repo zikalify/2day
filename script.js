edd24fab6570f74f7bef55f09552e273fe0f85aa
Paul Hill <zikalify@gmail.com>
2024-07-31 15:51:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -134,13 +134,13 @@ document.addEventListener("DOMContentLoaded", () => {
             document.body.style.backgroundColor = "#9B5D9B";
             message.style.backgroundColor = "#6A0D91";
             message.style.color = "#FFFFFF";
-            message.style.border = "5px solid #FFF826";
+            message.style.border = "5px solid #000000";
         } else {
             message.innerText = "Pregnancy is unlikely today";
             document.body.style.backgroundColor = "#FEFB87";
             message.style.backgroundColor = "#FFF826";
             message.style.color = "#000000";
-            message.style.border = "5px solid #6A0D91";
+            message.style.border = "5px solid #000000";
         }
     }
 
